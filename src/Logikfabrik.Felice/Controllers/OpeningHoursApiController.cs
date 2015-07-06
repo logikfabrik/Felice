2756c6f46c9ffcda4d40c5b43f612e4478df6a50
logikfabrik <anton@logikfabrik.se>
2015-07-06 20:00:51 +0200

    Added API controller for opening hours.

---
+++ src/Logikfabrik.Felice/Controllers/OpeningHoursApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace Logikfabrik.Felice.Controllers
+{
+    public class OpeningHoursApiController : UmbracoApiController
+    {
+        public JsonResult GetDays()
+        {
+            // TODO: Get days (localized) to the AngularJS editor.
+            throw new NotImplementedException();
+        }
+    }
+}
